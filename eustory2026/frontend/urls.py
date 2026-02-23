d23e2e077c4d595c3ba2d8ded04cfecd6da5ee76
GdXcIi <gdupuis92100@gmail.com>
2026-02-23 15:06:13 +0100

    Added OpenSoftware License

--- eustory2026/frontend/urls.py
+++ eustory2026/frontend/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import dashboard, login, subtheme
+from .views import dashboard, login, subtheme, subtheme_old
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('login/', login, name='login'),
+    path('subthemeold/<int:id>/', subtheme_old, name='subthemeold'),
     path('subtheme/<int:id>/', subtheme, name='subtheme'),
 ]
--- eustory2026/frontend/views.py
+++ eustory2026/frontend/views.py
@@ -7,5 +7,8 @@ def login(request):
 def dashboard(request):
     return render(request, 'dashboard.html')
 
+def subtheme_old(request, id):
+    return render(request, 'subtheme_old.html', {'id': id})
+
 def subtheme(request, id):
     return render(request, 'subtheme.html', {'id': id})
